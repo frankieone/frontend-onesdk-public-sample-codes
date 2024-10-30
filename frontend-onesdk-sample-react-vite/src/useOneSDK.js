6edc4c986110664532f4ea084261a44f81910cf7
nelvson <nelvson1337@gmail.com>
2024-10-31 06:40:46 +0700

    rename QATest to flows. add new advanced module flow

--- frontend-onesdk-sample-react-vite/src/useOneSDK.js
+++ frontend-onesdk-sample-react-vite/src/useOneSDK.js
@@ -35,7 +35,7 @@ const useOneSDK = ({config, alternateKey}) => {
           body: JSON.stringify({
               permissions: {
                   "preset": "one-sdk",
-                  "reference": `demo-${new Date().toISOString()}`//"<YOUR_UNIQUE_CUSTOMER_REF>"
+                  "reference": `demo-hello-there-general-kenobi`//"<YOUR_UNIQUE_CUSTOMER_REF>"
               }
           })
       });
