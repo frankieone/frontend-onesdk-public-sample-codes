ddc0d8a291ea43becc0ef4e79b53076b08d9c4ec
Nelvson <nelvson1337@gmail.com>
2024-11-13 08:08:51 +0700

    bump one-sdk npm version & add new flows (#4)
    * bump one-sdk npm version
    
    * rename QATest to flows. add new advanced module flow
    
    * add IDV Review page

--- frontend-onesdk-sample-react-vite/src/App.jsx
+++ frontend-onesdk-sample-react-vite/src/App.jsx
@@ -1,18 +1,19 @@
 "use client"
 import { useState } from 'react';
-import End2End from './QATest/End2End';
-import IDVIncode from './QATest/IDVIncode';
-import IDVIncodeDynamic from './QATest/IDVIncodeDynamic';
-import NewOnboarding from './QATest/NewOnboarding';
-import NewOnboardingManual from './QATest/NewOnboardingManual';
-import Onfido from './QATest/OnfidoNew';
-import Sardine from './QATest/Sardine';
-import End2EndOCR from './QATest/End2EndOCR';
-import IDVerse from './QATest/IDVerse';
-import SmartUI from './QATest/SmartUI';
+import End2End from './flows/End2End';
+import IDVIncode from './flows/IDVIncode';
+import IDVIncodeDynamic from './flows/IDVIncodeDynamic';
+import NewOnboarding from './flows/NewOnboarding';
+import NewOnboardingManual from './flows/NewOnboardingManual';
+import Onfido from './flows/OnfidoNew';
+import Sardine from './flows/Sardine';
+import End2EndOCR from './flows/End2EndOCR';
+import IDVerse from './flows/IDVerse';
+import SmartUI from './flows/SmartUI';
+import AdvancedKycForm from './flows/advanced-use-case/AdvancedKycForm'
 
 function App() {
-  const methodMapping = ['End2End', 'End2EndOCR', 'IDV Incode', 'IDV Incode Dynamic', 'IDVerse', 'New Onboarding', 'New Onboarding Manual', 'Onfido', 'Sardine', 'Smart UI']
+  const methodMapping = [ 'Advanced Kyc', 'End2End', 'End2EndOCR', 'IDV Incode', 'IDV Incode Dynamic', 'IDVerse', 'New Onboarding', 'New Onboarding Manual', 'Onfido', 'Sardine', 'Smart UI']
 
   const [selectedMethod, setSelectedMethod] = useState('')
   const handleOnClickMethod = (method) => {
@@ -20,6 +21,7 @@ function App() {
   }
   const renderOneSDK = () => {
     switch (selectedMethod) {
+      case 'Advanced Kyc': return <AdvancedKycForm />
       case 'End2End': return <End2End/>
       case 'End2EndOCR': return <End2EndOCR/>
       case 'IDV Incode': return <IDVIncode/>
